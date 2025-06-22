db0258439941903eb04c488943aa1ba9b3ced95e
Ivana Mariel Millosa <imbmillosa@gmail.com>
2025-06-23 06:00:19 +0800

    Change $ to ₱

--- sanity/schemaTypes/productType.ts
+++ sanity/schemaTypes/productType.ts
@@ -112,7 +112,7 @@ export const productType = defineType({
       const image = media && media[0];
       return {
         title: title,
-        subtitle: `$${subtitle}`,
+        subtitle: `₱${subtitle}`,
         media: image,
       };
     },
